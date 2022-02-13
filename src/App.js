287ba1d36a465a3778254fd2c562f59499090385
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-13 16:48:47 +0530

    Route Gaurding

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 
 function App() {
+    const { authIsReady, user } = useAuthContext();
     return (
         <div className="App">
-            <BrowserRouter>
-                <Navbar />
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/login">
-                        <Login />
-                    </Route>
-                    <Route path="/signup">
-                        <Signup />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
+            {authIsReady && (
+                <>
+                    <BrowserRouter>
+                        <Navbar />
+                        <Switch>
+                            <Route exact path="/">
+                                {!user && <Redirect to="/login" />}
+                                {user && <Home />}
+                            </Route>
+                            <Route path="/login">
+                                {user && <Redirect to="/" />}
+                                {!user && <Login />}
+                            </Route>
+                            <Route path="/signup">
+                                {user && <Redirect to="/" />}
+                                {!user && <Signup />}
+                            </Route>
+                        </Switch>
+                    </BrowserRouter>
+                </>
+            )}
         </div>
     );
 }
