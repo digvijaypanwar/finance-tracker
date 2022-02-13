e76fc2ec3267de36f6c2444dea237b8e1ad47e62
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-13 18:54:44 +0530

    Deleteing a transaction

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -24,6 +24,13 @@ const firestoreReducer = (state, action) => {
                 success: true,
                 error: null,
             };
+        case "DELETED_DOCUMENT":
+            return {
+                isPending: false,
+                document: null,
+                success: true,
+                error: null,
+            };
         case "ERROR":
             return {
                 isPending: false,
@@ -68,7 +75,20 @@ export const useFireStore = (collection) => {
     };
 
     //delete a doc
-    const deleteDocument = async (id) => {};
+    const deleteDocument = async (id) => {
+        dispatch({ type: "IS_PENDING" });
+        try {
+            await ref.doc(id).delete();
+            dispatchIfNotCancelled({
+                type: "DELETED_DOCUMENT",
+            });
+        } catch (err) {
+            dispatchIfNotCancelled({
+                type: "ERROR",
+                payload: "could not delete",
+            });
+        }
+    };
 
     useEffect(() => {
         return () => {
--- src/pages/Home/TransactionList.js
+++ src/pages/Home/TransactionList.js
@@ -1,12 +1,20 @@
+import { useFireStore } from "../../hooks/useFirestore";
 import styles from "./Home.module.css";
 
 function TransactionList({ transactions }) {
+    const { deleteDocument, response } = useFireStore("transactions");
+
+    console.log(response);
     return (
         <ul className={styles.transactions}>
             {transactions.map((transaction) => (
                 <li key={transaction.id}>
                     <p className={styles.name}>{transaction.name}</p>
                     <p className={styles.amount}>${transaction.amount}</p>
+
+                    <button onClick={() => deleteDocument(transaction.id)}>
+                        x
+                    </button>
                 </li>
             ))}
         </ul>
