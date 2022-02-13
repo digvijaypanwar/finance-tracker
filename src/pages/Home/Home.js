c531972c45d35f2fc0060ea04de1789f49cd476e
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-13 17:05:01 +0530

    Creating transaction form

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,14 @@
 import styles from "./Home.module.css";
+import TransactionForm from "./TransactionForm";
 
 function Home() {
-    return <div>Home1</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>transaction list</div>
+            <div className={styles.sidebar}>
+                <TransactionForm />
+            </div>
+        </div>
+    );
 }
 export default Home;
