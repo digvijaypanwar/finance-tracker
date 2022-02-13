5e56189f465a2e327e968a8f92e76a504fe24314
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-13 18:01:39 +0530

    creating useCollection for listing

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { projectFirestore } from "../firebase/config";
+
+export const useCollection = (collection) => {
+    const [documents, setDocuments] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        let ref = projectFirestore.collection(collection);
+
+        const unsub = ref.onSnapshot(
+            (snapshot) => {
+                let results = [];
+
+                snapshot.docs.forEach((doc) => {
+                    results.push({ ...doc.data(), id: doc.id });
+                });
+
+                setDocuments(results);
+                setError(null);
+            },
+            (err) => {
+                console.log(err);
+                setError("could not fetch data");
+            }
+        );
+        return () => {
+            unsub();
+        };
+    }, [collection]);
+
+    return { documents, error };
+};
