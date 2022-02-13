47189e2ccfc58bc73fd6084cf538d75307c0fc90
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-13 14:30:07 +0530

    adding and init firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB0iodTIUSuL6m6MDboKLZcwhgGvvA1clA",
+    authDomain: "mymoney-2c831.firebaseapp.com",
+    projectId: "mymoney-2c831",
+    storageBucket: "mymoney-2c831.appspot.com",
+    messagingSenderId: "482428467289",
+    appId: "1:482428467289:web:95d5dd460a18168d455948",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init service
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+export { projectFirestore, projectAuth };
