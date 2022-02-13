3a1ef58f9081760782606dd66502db8c42276e82
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-13 18:36:10 +0530

    sorting transactions by createdAt (desc)

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,19 +1,23 @@
 import { useEffect, useRef, useState } from "react";
 import { projectFirestore } from "../firebase/config";
 
-export const useCollection = (collection, _query) => {
+export const useCollection = (collection, _query, _orderBy) => {
     const [documents, setDocuments] = useState(null);
     const [error, setError] = useState(null);
 
     //breaking out of loop because of ref based variable
     //_query is an array and is different in every function call
     const query = useRef(_query).current;
+    const orderBy = useRef(_orderBy).current;
 
     useEffect(() => {
         let ref = projectFirestore.collection(collection);
 
         if (query) {
             ref = ref.where(...query);
+        }
+        if (orderBy) {
+            ref = ref.orderBy(...orderBy);
         }
         const unsub = ref.onSnapshot(
             (snapshot) => {
@@ -34,7 +38,7 @@ export const useCollection = (collection, _query) => {
         return () => {
             unsub();
         };
-    }, [collection, query]);
+    }, [collection, query, orderBy]);
 
     return { documents, error };
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,11 +6,11 @@ import TransactionList from "./TransactionList";
 
 function Home() {
     const { user } = useAuthContext();
-    const { error, documents } = useCollection("transactions", [
-        "uid",
-        "==",
-        user.uid,
-    ]);
+    const { error, documents } = useCollection(
+        "transactions",
+        ["uid", "==", user.uid],
+        ["createdAt", "desc"]
+    );
     return (
         <div className={styles.container}>
             <div className={styles.content}>
