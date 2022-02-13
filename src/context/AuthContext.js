f61fdf2128e2b9fce83f3ef766d3bf06e3e9f07f
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-13 15:36:06 +0530

    dispatching login action

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,8 +1,11 @@
 import { createContext, useReducer } from "react";
 
 export const AuthContext = createContext();
+
 export const authReducer = (state, action) => {
     switch (action.type) {
+        case "LOGIN":
+            return { ...state, user: action.payload };
         default:
             return state;
     }
@@ -11,6 +14,8 @@ export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null,
     });
+
+    console.log("AuthContext state:", state);
     return (
         <AuthContext.Provider value={{ ...state, dispatch }}>
             {children}
--- src/hooks/useAuthContext.js
+++ src/hooks/useAuthContext.js
@@ -1,9 +1,10 @@
 import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 export const useAuthContext = () => {
-    const context = useContext();
+    const context = useContext(AuthContext);
 
-    if (context) {
+    if (!context) {
         throw new Error("useAuthContext must be inside an AuthContextProvider");
     }
 
