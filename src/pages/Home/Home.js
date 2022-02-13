08baf22a1689bbbea0b66fe76dae785164f43da6
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-13 18:16:17 +0530

    Liting transactions on home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,18 @@
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
 import styles from "./Home.module.css";
 import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
 
 function Home() {
     const { user } = useAuthContext();
+    const { error, documents } = useCollection("transactions");
     return (
         <div className={styles.container}>
-            <div className={styles.content}>transaction list</div>
+            <div className={styles.content}>
+                {error && <p>{error}</p>}
+                {documents && <TransactionList transactions={documents} />}
+            </div>
             <div className={styles.sidebar}>
                 <TransactionForm uid={user.uid} />
             </div>
