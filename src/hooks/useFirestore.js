67f158eb16d4a00f95cba9c4160525186b812319
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-13 17:36:29 +0530

    updated reducer in useFirestore hook

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -10,6 +10,27 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
     switch (action.type) {
+        case "IS_PENDING":
+            return {
+                isPending: false,
+                document: null,
+                success: false,
+                error: null,
+            };
+        case "ADDED_DOCUMENT":
+            return {
+                isPending: false,
+                document: action.payload,
+                success: true,
+                error: null,
+            };
+        case "ERROR":
+            return {
+                isPending: false,
+                document: null,
+                success: false,
+                error: action.payload,
+            };
         default:
             return state;
     }
@@ -21,11 +42,32 @@ export const useFireStore = (collection) => {
     //collection ref
     const ref = projectFirestore.collection(collection);
 
+    //only dispatch if not cancelled
+    const dispatchIfNotCancelled = (action) => {
+        if (!isCancelled) {
+            dispatch(action);
+        }
+    };
+
     //add a doc
-    const addDocument = (doc) => {};
+    const addDocument = async (doc) => {
+        dispatch({ type: "IS_PENDING" });
+        try {
+            const addedDocument = await ref.add(doc);
+            dispatchIfNotCancelled({
+                type: "ADDED_DOCUMENT",
+                payload: addedDocument,
+            });
+        } catch (err) {
+            dispatchIfNotCancelled({
+                type: "ERROR",
+                payload: err.message,
+            });
+        }
+    };
 
     //delete a doc
-    const deleteDocument = (id) => {};
+    const deleteDocument = async (id) => {};
 
     useEffect(() => {
         return () => {
